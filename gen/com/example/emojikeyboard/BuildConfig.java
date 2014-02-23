0219e0bf0be5972c611b7aecfaf8928d1c857c09
Sam Rajan <samjerusalem@gmail.com>
2014-02-23 19:57:37 +0530

    First Commit

---
+++ gen/com/example/emojikeyboard/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.emojikeyboard;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Classic Style emoji keyboard with swipe tabs across categories of emojis..(Whatsapp Style)
